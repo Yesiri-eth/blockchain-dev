893ef3c7131d41272e7fc445cae84e940ef3b15e
Yesiri-eth <yesiriolatigbe@gmail.com>
2024-01-21 22:54:23 +0100

    Create odd_and_even_numberchecker.sol

---
+++ odd_and_even_numberchecker.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+  contract numChecker {
+ function oddChecker (uint256 number)public pure returns (string memory){
+if(number % 2 != 0 ) {return "it is odd" ;
+} else { return "it is even";
+
+}
+
+ }
+   
+  
+  
+  
+  }
+ 
