d921590a17a093b5bb61cdfa5fe4d028a6d165b4
Yesiri-eth <yesiriolatigbe@gmail.com>
2024-02-18 19:00:37 +0000

    nextt

---
+++ practic/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+   solidity: "0.8.24",
+   networks:{
+     "arbitrum-sepolia": {
+      url:"https://sepolia-rollup.arbitrum.io/rpc",
+      chainId: 421614,
+      accounts: [""]
+       
+     } 
+
+
+  },
+};
